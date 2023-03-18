bf57c31e694b4944ba501b609680628a15ebc4fd
baleeshka <balex.working@gmail.com>
2023-03-19 02:51:17 +0300

    refactor

--- localStorageSite/index.js
+++ localStorageSite/index.js
@@ -1,16 +1,3 @@
 import './index.css'
-
-const cookieMessage = document.querySelector('.cookie-consent')
-const button = document.querySelector('.cookie-consent__button')
-
-button.addEventListener('click', event => {
-	event.preventDefault()
-	localStorage.setItem('cookie', 'true')
-	cookieMessage.style.display = 'none'
-})
-
-if (localStorage.getItem('cookie') === 'true') {
-	cookieMessage.style.display = 'none'
-}
-
-//localStorage.clear()
+import initApp from './src/app'
+initApp()
